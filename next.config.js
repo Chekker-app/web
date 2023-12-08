0e72754dafccebb4f375795f89282b9ece8e6d07
fesilva-dev <feliper.silva011@gmail.com>
2023-12-08 17:35:13 -0300

    fix: adjust images and add favicon

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.freeimages.com', 'localhost', '192.168.1.100'],
+    domains: [
+      'images.freeimages.com',
+      'localhost',
+      '192.168.1.100',
+      'app.chekker.com.br',
+      'http://app.chekker.com.br',
+      'https://app.chekker.com.br',
+    ],
   },
 };
 
